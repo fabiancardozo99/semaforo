a40c8c2c156cab756ff4fde61491f4d9bba31ef8
Fabian <fabiancardozo99@gmail.com>
2022-10-24 11:13:22 -0300

    semaforo mejorado

--- semaforo.ino
+++ semaforo.ino
@@ -1,6 +1,7 @@
 #define LED_GREEN 23
 #define LED_YELLOW 22
 #define LED_RED 21
+#define PARPADEO 5
 
 void setup() {
   pinMode(LED_GREEN, OUTPUT);
@@ -8,20 +9,47 @@ void setup() {
   pinMode(LED_RED, OUTPUT);
 }
 
-void loop() {
+//Semaforo normal: Rojo - amarillo - verde
+//void loop() {
+//  //Rojo
+//  digitalWrite(LED_RED, HIGH);
+//  delay(1500);
+//  digitalWrite(LED_RED, LOW);
+//
+//  //Amarillo
+//  digitalWrite(LED_YELLOW, HIGH);
+//  delay(1500);
+//  digitalWrite(LED_YELLOW, LOW);
+//
+//  //Verde
+//  digitalWrite(LED_GREEN, HIGH);
+//  delay(1500);
+//  digitalWrite(LED_GREEN, LOW);
+//
+//}
+
+void loop(){
   //Rojo
   digitalWrite(LED_RED, HIGH);
   delay(1500);
   digitalWrite(LED_RED, LOW);
+
+   //Verde
+  digitalWrite(LED_GREEN, HIGH);
+  delay(1500);
+  //Parpadea n veces
+  for(int i = 0; i < PARPADEO; i++) {
+    delay(300);
+    digitalWrite(LED_GREEN, LOW);
+    delay(300);
+    digitalWrite(LED_GREEN, HIGH);
+  }
+   digitalWrite(LED_GREEN, LOW);
 
   //Amarillo
   digitalWrite(LED_YELLOW, HIGH);
   delay(1500);
   digitalWrite(LED_YELLOW, LOW);
 
-  //Verde
-  digitalWrite(LED_GREEN, HIGH);
-  delay(1500);
-  digitalWrite(LED_GREEN, LOW);
-
+  
 }
